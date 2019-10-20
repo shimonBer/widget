ebddcc4041998f257972d889c2fc9b9a7fbd1061
shimonbe <shimonbe@checkpoint.com>
2019-10-20 12:01:03 +0300

    change structure and add api

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-function printHello(){
-    console.log("gggfgfgd");
+var request = new XMLHttpRequest();
+request.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
+var data;
+
+request.onload = function () {
+
+    if (request.status >= 200 && request.status < 400){
+        data = JSON.parse(this.response);
+        console.log(data);
+    var count = 0;
+    document.querySelectorAll(".item").forEach(item => {
+    var itemTitle = item.querySelector(".item-title");
+    var itemBrand = item.querySelector(".item-brand");
+    var itemThumbnail = item.querySelector(".thumbnail");
+    if ('textContent' in itemTitle) { //for  Firefox
+        itemTitle.textContent = data[count].title;
+        itemBrand.textContent = data[count].id;
+    } else { // for IE8
+        itemTitle.innerText = data[count].title;
+        itemBrand.innerText = data[count].id;
+    }
+    itemThumbnail.src = data[count].thumbnailUrl;
+    count ++;
+});
+    
+
+    }
+
+    else{
+        //throw error
+    }
+    
 }
+
+   
+   // Send request
+request.send();
