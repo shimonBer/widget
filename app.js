10eaf9a8bb280a8377e3ca9e5eddc87467d1728a
shimonbe <shimonbe@checkpoint.com>
2019-10-20 13:15:45 +0300

    add error handeling

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
-var data;
+try {
+    var request = new XMLHttpRequest();
 
-request.onload = function () {
+    request.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400){
+            var data = JSON.parse(this.response);
+            var itemNum = 0;
+            document.querySelectorAll(".item").forEach(item => {
+            var itemTitle = item.querySelector(".item-title");
+            var itemBrand = item.querySelector(".item-brand");
+            var itemThumbnail = item.querySelector(".thumbnail");
+            if ('textContent' in itemTitle) { //for  Firefox
+                itemTitle.textContent = data[itemNum].title;
+                itemBrand.textContent = data[itemNum].id;
+            } else { // for IE8
+                itemTitle.innerText = data[itemNum].title;
+                itemBrand.innerText = data[itemNum].id;
+            }
+            itemThumbnail.src = data[itemNum].thumbnailUrl;
+            itemNum ++;
+            });
 
-    if (request.status >= 200 && request.status < 400){
-        data = JSON.parse(this.response);
-        console.log(data);
-    var count = 0;
-    document.querySelectorAll(".item").forEach(item => {
-    var itemTitle = item.querySelector(".item-title");
-    var itemBrand = item.querySelector(".item-brand");
-    var itemThumbnail = item.querySelector(".thumbnail");
-    if ('textContent' in itemTitle) { //for  Firefox
-        itemTitle.textContent = data[count].title;
-        itemBrand.textContent = data[count].id;
-    } else { // for IE8
-        itemTitle.innerText = data[count].title;
-        itemBrand.innerText = data[count].id;
-    }
-    itemThumbnail.src = data[count].thumbnailUrl;
-    count ++;
-});
+
+        }
+
+        else{
+            console.error("Err when requesting data");
+        }
     
+    }   
+    request.send();
 
-    }
+  } catch (err) { 
+    console.error(`${err} occurred, message: ${err.message}`); 
+  
+  }
 
-    else{
-        //throw error
-    }
-    
-}
 
-   
-   // Send request
-request.send();
+
